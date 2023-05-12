a69cc4d5d402ea5f754d58bad4a57066aba8870f
Sergius Novikov <kraspvp@mail.ru>
2023-05-12 21:10:57 +0300

    -

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ brew install perl lua
 brew install qemu minikube kubectl helm
 brew install fzf lf tig
 brew install openssl gcc automake libtool autoconf cmake
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
