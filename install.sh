be4ff7f4cc0338d50748cc9695733369020ac013
Sergius Novikov <kraspvp@mail.ru>
2023-05-12 20:45:08 +0300

    -

---
+++ install.sh
@@ -0,0 +1,6 @@
+brew install jq tmux zsh neovim
+brew install wget telnet nmap netcat
+brew install perl lua
+brew install qemu minikube kubectl helm
+brew install fzf lf tig
+brew install openssl gcc automake libtool autoconf cmake
