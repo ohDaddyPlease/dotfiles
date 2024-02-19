b66553cdf83067d4c3068905240b2293c3cbbc23
Sergius Novikov <kraspvp@mail.ru>
2024-02-20 01:04:56 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 brew install jq tmux zsh neovim
-brew install wget telnet nmap netcat
-brew install perl lua
-brew install qemu minikube kubectl helm
-brew install fzf lf tig
+brew install wget telnet nmap netcat mitmproxy
+brew install perl lua go php
+brew install qemu minikube kubectl helm docker docker-compose docker-desktop
+brew install fzf lf tig ag htop
 brew install openssl gcc automake libtool autoconf cmake
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
